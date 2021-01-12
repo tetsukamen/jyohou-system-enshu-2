4c82ba60e868639fc77a0e5a39120e9d6bde4f9f
tetsuyawada <tetsukamen00@gmail.com>
2021-01-12 13:53:50 +0900

    入力文字のカンマ、改行コードに対応

--- webserver/html/index.php
+++ webserver/html/index.php
@@ -1,20 +1,27 @@
 <?php
+// レスポンスヘッダの設定
+header("HTTP/1.0 200 OK");
+header("Server: Apache");
+header("Content-Type: text/html");
+
 // 初期設定
 date_default_timezone_set('Asia/Tokyo'); // タイムゾーンの設定
 
 // ポストされた情報をログファイルに保存
 if ($_POST) {
-    $data = array($_POST['name'],$_POST['comment'],date("Y/m/d H:i:s"));
+    // カンマ、改行コードを置き換え
+    $name = $_POST['name'];
+    $name = str_replace(",", "&#44", $name);
+    $comment = $_POST['comment'];
+    $comment = str_replace(array("\r\n","\r","\n"), '<br>', $comment);
+    $comment = str_replace(",", "&#44", $comment);
+    // データをcsvに書き込み
+    $data = array($name,$comment,date("Y/m/d H:i:s"));
     $line = implode(',', $data);
     $fp = fopen('log.csv', 'a');
     fwrite($fp, $line."\n");
     fclose($fp);
 }
-
-// レスポンスヘッダの設定
-header("HTTP/1.0 200 OK");
-header("Server: Apache");
-header("Content-Type: text/html");
 ?>
 
 <!DOCTYPE HTML>

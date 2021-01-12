4b943399c657bdb454d620d426419873b7faa088
tetsuyawada <tetsukamen00@gmail.com>
2021-01-12 15:31:50 +0900

    ページネーション実装

--- webserver/html/index.php
+++ webserver/html/index.php
@@ -32,20 +32,8 @@ if ($_POST) {
 </head>
 
 <body>
+
 <h1>シンプル掲示板</h1>
-<?php
-$row = 1;
-// ファイルが存在しているかチェックする
-if (($handle=fopen('log.csv', 'r'))!==false) {
-    // 1行ずつfgetcsv()関数を使って読み込む
-    while ($data=fgetcsv($handle)) {
-        echo "<p> $row 名前 : <span class='name'> $data[0] </span> Date: $data[2] <br>$data[1]</p>";
-        $row++;
-    }
-    fclose($handle);
-}
-?>
-</p>
 
 <form method="POST" action="/">
     名前<br>
@@ -54,5 +42,60 @@ if (($handle=fopen('log.csv', 'r'))!==false) {
     <textarea name="comment" id="" cols="50" rows="5"></textarea><br>
     <input type="submit" value="書き込む">
 </form>
+
+<?php
+// csvのデータを変数に格納
+$posts = [];
+$idx = 0;
+// ファイルが存在しているかチェックする
+if (($handle=fopen('log.csv', 'r'))!==false) {
+    // 1行ずつfgetcsv()関数を使って読み込む
+    while ($data=fgetcsv($handle)) {
+        $posts[] = array($idx+1,$data[0],$data[1],$data[2]);
+        $idx++;
+    }
+    fclose($handle);
+}
+// 変数に格納したデータを表示
+$count = count($posts);
+// ページ設定
+$page = 1; // デフォルトは１ページを表示
+if ($_GET) {
+    if ($_GET['page']) {
+        $page = $_GET['page'];
+        $count = $count - ($page-1)*10;
+    }
+}
+for ($i=0;$i<10;$i++) {
+    $post_num = $count;
+    $count--;
+    if ($count>=0) {
+        $data = $posts[$count];
+        echo "<p> $post_num 名前 : <span class='name'> $data[0] </span> Date: $data[2] <br>$data[1]</p>";
+    }
+}
+?>
+
+<!-- ページネーション -->
+<div>
+<?php
+// 前へ戻るリンク
+if ($page>1) {
+    $back = $page-1;
+    echo "<a href='/?page=".$back."'>戻る</a>";
+}
+// ページ番号指定リンク
+$page_count = ceil(count($posts)/10);
+for ($i=1;$i<=$page_count;$i++) {
+    echo "<a href='/?page=".$i."'>".$i."</a>";
+}
+// 次へ進むリンク
+if ($page<$page_count) {
+    $next = $page+1;
+    echo "<a href='/?page=".$next."'>次へ</a>";
+}
+?>
+</div>
+
 </body>
 </html>
